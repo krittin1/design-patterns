06d73809eea6467d495a41945e379dbbea97ef6a
Arty                Kanok <krittin.kan1@ku.th>
2019-09-05 03:18:27 +0700

    Delete GuessingGame.java

--- src/GuessingGame/GuessingGame.java
+++
@@ -1,8 +0,0 @@
-public interface GuessingGame {
-
-        void setAnswer(String ans);
-        void guess(String s);
-        String getOutput();
-        boolean isWon();
-        boolean isLost();
-}
